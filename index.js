95b2ab1497184f45fa2b6578e365ba95aac04dd6
Sanket6652 <sanketnannaware96@gmail.com>
2024-08-17 00:32:30 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+  });
+
+// Simple route
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
